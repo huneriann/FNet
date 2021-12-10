8900dd2dd525dea1fbc1627b51fe08655c486311
hunerianncode <hesimovilkin@gmail.com>
2021-12-10 23:01:07 +0400

    Redrawed DetailsPage`s TreeView. Added logic for directory/file TreeViewItem

--- FNet.FSA.WPF/DetailsPage.xaml.cs
+++ FNet.FSA.WPF/DetailsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,8 +11,8 @@ namespace FNet.FSA.WPF
     /// </summary>
     public partial class DetailsPage : Page, INotifyPropertyChanged
     {
-        private Core.Model.PathInfo pathInfo;
-        public Core.Model.PathInfo PathInfo
+        private ObservableCollection<Core.Model.PathInfo> pathInfo;
+        public ObservableCollection<Core.Model.PathInfo> PathInfo
         {
             get { return pathInfo; }
             set { pathInfo = value; OnChanged(); }
@@ -22,12 +23,62 @@ namespace FNet.FSA.WPF
             InitializeComponent();
             this.DataContext = this;
 
-            PathInfo = new Core.Model.PathInfo();
+            PathInfo = new ObservableCollection<Core.Model.PathInfo>();
         }
 
         public void UpdateInterface(Core.Model.PathInfo info)
         {
-            PathInfo = info;
+            mainTreeView.Items.Clear();
+
+            TreeViewItem pathTreeViewItem = new TreeViewItem();
+            pathTreeViewItem.Header = "Path Info";
+
+            var pathStateTxtBlock = new TextBlock();
+            pathStateTxtBlock.Text = "Path: " + info.State;
+            var dateTimeTxtBlock = new TextBlock();
+            dateTimeTxtBlock.Text = "Date Time: " + info.DateTime.ToString("dd.MM.yyyy HH:mm:ss");
+            var infoTxtBlock = new TextBlock();
+            infoTxtBlock.Text = "Info: " + info.Info;
+
+            pathTreeViewItem.Items.Add(infoTxtBlock);
+            pathTreeViewItem.Items.Add(pathStateTxtBlock);
+            pathTreeViewItem.Items.Add(dateTimeTxtBlock);
+
+            var subTreeViewItem = new TreeViewItem();
+            if (new System.IO.DirectoryInfo(info.Path).Exists)
+            {
+                subTreeViewItem.Header = "Directory Info";
+
+                subTreeViewItem.Items.Add("Full Path: " + info.DirectoryInfo.FullName);
+                subTreeViewItem.Items.Add("File Path: " + info.DirectoryInfo.Name);
+                subTreeViewItem.Items.Add("Attributes: " + info.DirectoryInfo.Attributes);
+                subTreeViewItem.Items.Add("Creation DateTime: " + info.DirectoryInfo.CreationTime.ToString("dd.MM.yyyy HH:mm:ss"));
+                subTreeViewItem.Items.Add("Extension: " + info.DirectoryInfo.Extension);
+                subTreeViewItem.Items.Add("Last Modified DateTime: " + info.DirectoryInfo.LastAccessTime.ToString("dd.MM.yyyy HH:mm:ss"));
+                subTreeViewItem.Items.Add("Parent: " + info.DirectoryInfo.Parent);
+                subTreeViewItem.Items.Add("Root: " + info.DirectoryInfo.Root);
+                subTreeViewItem.Items.Add("Subdirectories Count: " + info.DirectoryInfo.GetDirectories().Length);
+                subTreeViewItem.Items.Add("Files Count: " + info.DirectoryInfo.GetFiles().Length);
+            }
+            if (new System.IO.FileInfo(info.Path).Exists)
+            {
+                subTreeViewItem.Header = "File Info";
+
+                subTreeViewItem.Items.Add("Full Path: " + info.FileInfo.FullName);
+                subTreeViewItem.Items.Add("File Path: " + info.FileInfo.Name);
+                subTreeViewItem.Items.Add("Directory Path: " + info.FileInfo.DirectoryName);
+                subTreeViewItem.Items.Add("Extension: " + info.FileInfo.Extension);
+                subTreeViewItem.Items.Add("Attributes: " + info.FileInfo.Attributes);
+                subTreeViewItem.Items.Add("Creation DateTime: " + info.FileInfo.CreationTime.ToString("dd.MM.yyyy HH:mm:ss"));
+                subTreeViewItem.Items.Add("Is Read Only: " + info.FileInfo.IsReadOnly.ToString());
+                subTreeViewItem.Items.Add("Last Modified DateTime: " + info.FileInfo.LastAccessTime.ToString("dd.MM.yyyy HH:mm:ss"));
+                subTreeViewItem.Items.Add("Size (bytes): " + info.FileInfo.Length.ToString());
+            }
+
+            subTreeViewItem.IsExpanded = true;
+            pathTreeViewItem.Items.Add(subTreeViewItem);
+            pathTreeViewItem.IsExpanded = true;
+            mainTreeView.Items.Add(pathTreeViewItem);
         }
 
         private void expandBtn_Click(object sender, RoutedEventArgs e)
